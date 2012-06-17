5b6fa995e458ee81ff5f20d80746b04857a37f36
thebruce87m <brian6@gmail.com>
2012-06-17 10:25:32 +0100

    adding in some c files

---
+++ c code/xcode/hello world/hello world/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  hello world
+//
+//  Created by Brian Lee on 17/06/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    
+    getchar();
+    
+    return 0;
+}
+
